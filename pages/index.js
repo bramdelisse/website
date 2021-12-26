a3c19cfc0daebc36df8c716cf4af7ebaf1bdc2f6
Bram Delisse <80391302+bramdelisse@users.noreply.github.com>
2021-12-26 23:04:53 +0100

    Change favicon & main text

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my website!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Ik doe mijn best hier wat moois te krijgen, maar het is nog allemaal nieuw voor me :)
         </p>
       </main>
 

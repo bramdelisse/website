d879ebbc3f4948fff7bac49156fbaac71638665f
Bram Delisse <80391302+bramdelisse@users.noreply.github.com>
2021-12-26 23:19:16 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Brems website!</title>
+        <link rel="shortcut icon" type="image/png"  href="/favicon.png" />
       </Head>
 
       <main>

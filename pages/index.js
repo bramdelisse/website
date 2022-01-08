d94671e8faf89c3fbee228b37f50861170aa7652
Bram Delisse <80391302+bramdelisse@users.noreply.github.com>
2022-01-08 15:40:01 +0100

    First effort to make index.html and globals.css my own

--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,24 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Brems website!</title>
-        <link rel="shortcut icon" type="image/png"  href="/favicon.png" />
+        <link rel="shortcut icon" type="image/png"  href="./favicon.png" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my website!" />
-        <p className="description">
-          Current album recommendations:
-          <hr />
-          <ul>
-            <li><a title="album_of_the_week" href="https://open.spotify.com/album/2zUWGNHT5fGrNW52apQa9q?si=760s8UcLT12BFv-HC6mw_A" target="_blank" rel="noopener noreferrer">Hayden Calnin - What It Means to Be Human</a> </li>
-            <li><a title="album_of_the_week" href="https://open.spotify.com/album/0fk3Dd2Cz7esxje40DHoQQ?si=Na5lEB5KSv22yrRbmsSBOQ" target="_blank" rel="noopener noreferrer">Hundredth - Somewhere Nowhere</a> </li>
-            <li><a title="album_of_the_week" href="https://open.spotify.com/album/0SFtIrRytNI4kcf93Tbhdf?si=74e65c03b1e845b0" target="_blank" rel="noopener noreferrer">Fred again... - Actual Life 2</a> </li>
-          </ul>
-        </p>
-      </main>
+      <body>
+        <div class="container">
 
-      <Footer />
+          <div class="box-1">
+            <h1>Mijn visie</h1>
+            <p>Mijn persoonlijke visie</p>
+          </div>
+
+          <div class="box-2">
+            <h2>Wie ben ik</h2>
+            <p>Brem!</p>
+          </div>
+
+        </div>
+      </body>
     </div>
   )
 }

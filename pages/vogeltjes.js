8d73e843adbca1eca3d0422db56e7e7df6d6796f
Bram Delisse <80391302+bramdelisse@users.noreply.github.com>
2022-01-12 14:04:18 +0100

    First index template with squares

---
+++ pages/vogeltjes.js
@@ -0,0 +1,15 @@
+export default function vogeltjes() {
+    return (
+        <>
+            <head>
+                <title>
+                    Vogeltjes
+                </title>
+            </head>
+
+            <body>
+                <h1>De vogeltjes</h1>
+            </body>
+        </>
+    )
+}
